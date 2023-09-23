c1c338bd17b5d4f15756883268a049d7c890b94d
unknown <hossein.singer7@gmai.com>
2023-09-23 19:12:05 +0330

    commit 2

--- components/cnversation.vue
+++ components/cnversation.vue
@@ -1,69 +1,110 @@
 <template>
-   <div class="main">
-    <div class="users">
-        <div class="user-title">گفتگوها</div>
-        <div class="user-item">
-            <div class="user-info">
-                <img class="user-pic" src="/Images/conversation/image5.svg">
-                <div class="user-text">
-                    <h5 class="user-name">مرجان خلیل پور</h5>
-                    <p class="user-date">۱۴۰۰/۱۲/۰۲ |  ۱۲:۳۴</p>
+    <div class="main">
+        <div class="users">
+            <div class="user-title">گفتگوها</div>
+            <div class="user-item">
+                <div class="user-info">
+                    <img class="user-pic" src="/Images/conversation/image5.svg">
+                    <div class="user-text">
+                        <h5 class="user-name">مرجان خلیل پور</h5>
+                        <p class="user-date">۱۴۰۰/۱۲/۰۲ | ۱۲:۳۴</p>
+                    </div>
                 </div>
+                <div class="user-link">6</div>
             </div>
-            <div class="user-link">6</div>
+            <div class="user-item">
+                <div class="user-info">
+                    <img class="user-pic" src="/Images/conversation/Users.svg">
+                    <div class="user-text">
+                        <h5 class="user-name">مرجان خلیل پور</h5>
+                        <p class="user-date">۱۴۰۰/۱۲/۰۲ | ۱۲:۳۴</p>
+                    </div>
+                </div>
+                <div class="user-link">6</div>
+            </div>
+            <div class="user-item">
+                <div class="user-info">
+                    <img class="user-pic" src="/Images/conversation/image6.svg">
+                    <div class="user-text">
+                        <h5 class="user-name">مرجان خلیل پور</h5>
+                        <p class="user-date">۱۴۰۰/۱۲/۰۲ | ۱۲:۳۴</p>
+                    </div>
+                </div>
+                <div class="user-link">6</div>
+            </div>
+            <div class="user-item">
+                <div class="user-info">
+                    <img class="user-pic" src="/Images/conversation/image7.svg">
+                    <div class="user-text">
+                        <h5 class="user-name">مرجان خلیل پور</h5>
+                        <p class="user-date">۱۴۰۰/۱۲/۰۲ | ۱۲:۳۴</p>
+                    </div>
+                </div>
+                <div class="user-link">6</div>
+            </div>
+            <div class="user-item">
+                <div class="user-info">
+                    <img class="user-pic" src="/Images/conversation/image8.svg">
+                    <div class="user-text">
+                        <h5 class="user-name">مرجان خلیل پور</h5>
+                        <p class="user-date">۱۴۰۰/۱۲/۰۲ | ۱۲:۳۴</p>
+                    </div>
+                </div>
+                <div class="user-link">6</div>
+            </div>
+            <div class="user-item">
+                <div class="user-info">
+                    <img class="user-pic" src="/Images/conversation/image9.svg">
+                    <div class="user-text">
+                        <h5 class="user-name">مرجان خلیل پور</h5>
+                        <p class="user-date">۱۴۰۰/۱۲/۰۲ | ۱۲:۳۴</p>
+                    </div>
+                </div>
+                <div class="user-link">6</div>
+            </div>
         </div>
-        <div class="user-item">
-            <div class="user-info">
-                <img class="user-pic" src="/Images/conversation/Users.svg">
-                <div class="user-text">
-                    <h5 class="user-name">مرجان خلیل پور</h5>
-                    <p class="user-date">۱۴۰۰/۱۲/۰۲ |  ۱۲:۳۴</p>
+        <div class="chat">
+            <div class="chat-userName">
+                <div class="chat-profile">
+                    <img class="profile-img" src="/Images/conversation/image5.svg">
+                    <div class="profile-info">
+                        <h6 class="profile-name">مرجان خلیل پور</h6>
+                        <span class="profile-level">سطح ۲ کاربری</span>
+                    </div>
                 </div>
             </div>
-            <div class="user-link">6</div>
-        </div>
-        <div class="user-item">
-            <div class="user-info">
-                <img class="user-pic" src="/Images/conversation/image6.svg">
-                <div class="user-text">
-                    <h5 class="user-name">مرجان خلیل پور</h5>
-                    <p class="user-date">۱۴۰۰/۱۲/۰۲ |  ۱۲:۳۴</p>
+            <div class="note-me">
+                <div class="chat-me">
+                    <div class="chat-text">سلام!<br>
+                        لطفا رسید پرداختتون رو برام ارسال کنید</div>
+                    <div class="chat-date"> 22:34am</div>
                 </div>
             </div>
-            <div class="user-link">6</div>
-        </div>
-        <div class="user-item">
-            <div class="user-info">
-                <img class="user-pic" src="/Images/conversation/image7.svg">
-                <div class="user-text">
-                    <h5 class="user-name">مرجان خلیل پور</h5>
-                    <p class="user-date">۱۴۰۰/۱۲/۰۲ |  ۱۲:۳۴</p>
+            <div class="date-chats">12June
+            </div>
+            <div class="chat-user">
+                <div class="chat-user-border">
+                    <div class="chat-user-text">سلاممم!!</div>
+                    <div class="chat-date"> 22:34am</div>
+                </div>
+                <div class="chat-user-border">
+                    <div class="chat-user-text">چشم حتما!!</div>
+                    <div class="chat-date"> 22:34am</div>
+                </div>
+                <div class="chat-user-border">
+                    <img  src="/Images/conversation/image10.svg">
+                    <div class="chat-date"> 22:34am</div>
                 </div>
             </div>
-            <div class="user-link">6</div>
-        </div>
-        <div class="user-item">
-            <div class="user-info">
-                <img class="user-pic" src="/Images/conversation/image8.svg">
-                <div class="user-text">
-                    <h5 class="user-name">مرجان خلیل پور</h5>
-                    <p class="user-date">۱۴۰۰/۱۲/۰۲ |  ۱۲:۳۴</p>
-                </div>
+            
+            <div class="send-input">
+                <input class="chat-input" type="text" >
+                  <div class="send"><img src="/Images/footer/telegram.svg" alt=""></div>
+                  <div class="attach"><img src="/Images/conversation/attach.svg"></div>
             </div>
-            <div class="user-link">6</div>
-        </div>
-        <div class="user-item">
-            <div class="user-info">
-                <img class="user-pic" src="/Images/conversation/image9.svg">
-                <div class="user-text">
-                    <h5 class="user-name">مرجان خلیل پور</h5>
-                    <p class="user-date">۱۴۰۰/۱۲/۰۲ |  ۱۲:۳۴</p>
-                </div>
-            </div>
-            <div class="user-link">6</div>
+                
         </div>
     </div>
-   </div>
 </template>
 
 <style>
